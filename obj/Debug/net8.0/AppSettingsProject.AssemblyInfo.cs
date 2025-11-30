f9fcf0a4d59aa19449b7fe4c7e76c8711b808273
mazenAhmedAg <mazen.ahmed@belalsoft.com>
2025-11-30 19:27:09 +0300

    PUSH

--- obj/Debug/net8.0/AppSettingsProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/AppSettingsProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppSettingsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9832d4d4a9ba4e14ec6b68086d57e7b5d7c3c184")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a4f9e12ea0368779cf02697bd788244ee034556")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppSettingsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppSettingsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
